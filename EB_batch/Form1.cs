ab01893022a232bbbfece78fc579a3772c13af52
Administrator <Administrator@KCXFMFCRJRNWRNA>
2023-03-17 00:17:15 +0800

    第一版测试

--- EB_batch/Form1.cs
+++ EB_batch/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Dynamic;
 
 namespace EB_batch
 {
@@ -80,17 +81,78 @@ namespace EB_batch
         const int MOUSEEVENTF_MIDDLEUP = 0x0040;
         //标示是否采用绝对坐标
         const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rectangle rect);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateDC(
+         string lpszDriver,         // driver name驱动名
+         string lpszDevice,         // device name设备名
+         string lpszOutput,         // not used; should be NULL
+         IntPtr lpInitData   // optional printer data
+         );
+        [DllImport("gdi32.dll")]
+        public static extern int BitBlt(
+         IntPtr hdcDest, // handle to destination DC目标设备的句柄
+         int nXDest,   // x-coord of destination upper-left corner目标对象的左上角的X坐标
+         int nYDest,   // y-coord of destination upper-left corner目标对象的左上角的Y坐标
+         int nWidth,   // width of destination rectangle目标对象的矩形宽度
+         int nHeight, // height of destination rectangle目标对象的矩形长度
+         IntPtr hdcSrc,   // handle to source DC源设备的句柄
+         int nXSrc,    // x-coordinate of source upper-left corner源对象的左上角的X坐标
+         int nYSrc,    // y-coordinate of source upper-left corner源对象的左上角的Y坐标
+         UInt32 dwRop   // raster operation code光栅的操作值
+         );
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleDC(
+         IntPtr hdc // handle to DC
+         );
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(
+         IntPtr hdc,         // handle to DC
+         int nWidth,      // width of bitmap, in pixels
+         int nHeight      // height of bitmap, in pixels
+         );
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(
+         IntPtr hdc,           // handle to DC
+         IntPtr hgdiobj    // handle to object
+         );
+        [DllImport("gdi32.dll")]
+        public static extern int DeleteDC(
+         IntPtr hdc           // handle to DC
+         );
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(
+         IntPtr hwnd,                // Window to copy,Handle to the window that will be copied.
+         IntPtr hdcBlt,              // HDC to print into,Handle to the device context.
+         UInt32 nFlags               // Optional flags,Specifies the drawing options. It can be one of the following values.
+         );
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(
+         IntPtr hwnd
+         );
+
+
+
+
+
+
+
+
+
+
         #endregion
 
         Process process = null;
+        int zero_count = 0;
         IntPtr appWin;
         private string exeName = "";
         string dir;
         public Form1()
         {
             InitializeComponent();
-            CheckForIllegalCrossThreadCalls = false;
-            
+            CheckForIllegalCrossThreadCalls = false;            
         }
         private void KillProcess(string processName)
         {
@@ -120,17 +182,10 @@ namespace EB_batch
 
         private void button1_Click(object sender, EventArgs e)
         {
+            clear_status();
             openFileDialog1.ShowDialog();
         }
-        public void get_cpu(string ebs)
-        {
 
-        }
-
-        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
-        {
-
-        }
         public void clear_status()
         {
             listBox1.Items.Clear();
@@ -161,11 +216,23 @@ namespace EB_batch
             }
             //listBox1.Items.Add(filename);
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            exeName = textBox1.Text;
-            backgroundWorker1.RunWorkerAsync();
+            if (button2.Text == "开始")
+            {
+                exeName = textBox1.Text;
+                backgroundWorker1.RunWorkerAsync();
+                button2.Text = "停止";
+                button1.Enabled = false;
+            }
+            else
+            {
+                backgroundWorker1.CancelAsync();
+                button2.Text = "开始";
+                KillProcess("Ebsynth");
+                button1.Enabled = true;
+                toolStripProgressBar1.Value = 0;
+            }
         }
         public void workload(string ebsfilename)
         {
@@ -186,6 +253,53 @@ namespace EB_batch
             //SetWindowLong(appWin, GWL_STYLE, WS_VISIBLE);
             // Move the window to overlay it on this window
             MoveWindow(appWin, 0, 0, panel1.Width, panel1.Height, true);
+
+            Bitmap bmp=PrtWindow(appWin);
+            Color old = bmp.GetPixel(650, 750);
+            int i;
+            for (i = 750; i > 0; i--)
+            {
+                if (bmp.GetPixel(650, i) != old)
+                {
+                    System.Console.WriteLine(i);
+                    break;
+                }                  
+            }
+            int offsetx = this.Location.X;
+            int offsety = this.Location.Y;
+            MouseClick(690+ offsetx, 220 + offsety);
+            System.Threading.Thread.Sleep(100);
+            MouseClick(650 + offsetx, i+10 + offsety);
+            System.Threading.Thread.Sleep(100);
+            SetCursorPos(offsetx, offsety);
+        }
+       
+        public static Bitmap PrtWindow(IntPtr hWnd)
+
+        {
+
+            IntPtr hscrdc = GetWindowDC(hWnd);
+
+            var rect=new Rectangle();
+
+            GetWindowRect(hWnd, ref rect);
+
+            IntPtr hbitmap = CreateCompatibleBitmap(hscrdc, rect.Width, rect.Height);
+
+            IntPtr hmemdc = CreateCompatibleDC(hscrdc);
+
+            SelectObject(hmemdc, hbitmap);
+
+            PrintWindow(hWnd, hmemdc, 0);
+
+            Bitmap bmp = Bitmap.FromHbitmap(hbitmap);
+
+            DeleteDC(hscrdc);
+
+            DeleteDC(hmemdc);
+
+            return bmp;
+
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -195,7 +309,20 @@ namespace EB_batch
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            workload(dir + listBox1.Items[0].ToString());
+            int num = listBox1.Items.Count;
+            for (int i = 0; i < num; i++)
+            {
+                toolStripProgressBar1.Value = i*100 / num;
+                workload(dir + listBox1.Items[i].ToString());
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    if (zero_count > 5)
+                        break;
+                }
+                KillProcess("Ebsynth");
+            }
+
         }
         //执行点击事件
         private void MouseClick(int x,int y)
@@ -204,13 +331,46 @@ namespace EB_batch
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);      
         }
-        
-        private void button3_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            int offsetx = this.Location.X;
-            int offsety = this.Location.Y;
+            try //懒人写法 抛异常省代码。。
+            {
+                string name = "EbSynth";
+                var cpu = new PerformanceCounter("Process", "% Processor Time", name, true);
 
-            MouseClick(int.Parse(textBox2.Text)+offsetx, int.Parse(textBox3.Text)+offsety);
+                // Getting first initial values
+                cpu.NextValue();
+
+                // Creating delay to get correct values of CPU usage during next query
+
+                dynamic result = new ExpandoObject();
+
+                // If system has multiple cores, that should be taken into account
+                result.CPU = Math.Round(cpu.NextValue() / Environment.ProcessorCount, 2);
+                // Returns number of MB consumed by application
+                toolStripStatusLabel2.Text = "CPU占用：" + result.CPU;
+                if (result.CPU == 0)
+                    zero_count++;
+                else
+                    zero_count = 0;
+                //System.Console.WriteLine(name +" "+ result.CPU);
+
+            }
+            catch
+            {
+                toolStripStatusLabel2.Text = "CPU占用：00";
+            }
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Change the color of the link text by setting LinkVisited
+            // to true.
+            linkLabel1.LinkVisited = true;
+            //Call the Process.Start method to open the default browser
+            //with a URL:
+            System.Diagnostics.Process.Start("https://github.com/willwill85/EB_batch");
         }
     }
 }
